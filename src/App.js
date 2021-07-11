045a1947376b946429a7ca411c2a0c94512d902b
Manuel Peyon <57277595+PeyonManuel@users.noreply.github.com>
2021-07-11 17:17:19 -0300

    100% done

--- src/App.js
+++ src/App.js
@@ -12,10 +12,8 @@ function App() {
       document.querySelector('body').scrollTo(window.screen.width, 0);
     }
     document.querySelector('body').style.scrollBehavior = 'smooth';
-    document.querySelector('html').style.scrollBehavior = 'smooth';
 
     var xDown = null;
-    var yDown = null;
 
     const getTouches = (evt) => {
       return evt.touches || evt.originalEvent.touches;
@@ -24,33 +22,95 @@ function App() {
     const handleTouchStart = (evt) => {
       const firstTouch = getTouches(evt)[0];
       xDown = firstTouch.clientX;
-      yDown = firstTouch.clientY;
     };
 
     const handleTouchMove = (evt) => {
-      if (!xDown || !yDown) {
+      if (!xDown) {
         return;
       }
 
       var xUp = evt.touches[0].clientX;
-      var yUp = evt.touches[0].clientY;
 
       var xDiff = xDown - xUp;
-      var yDiff = yDown - yUp;
 
-      if (Math.abs(xDiff) > Math.abs(yDiff)) {
-        if (xDiff > 9) {
-          document
-            .querySelector('body')
-            .scrollTo(window.pageXOffset + window.screen.width, 0);
-        } else if (xDiff < -9) {
-          document
-            .querySelector('body')
-            .scrollTo(window.pageXOffset - window.screen.width, 0);
+      if (xDiff > 9) {
+        if (
+          document.body.scrollLeft + window.screen.width ===
+          window.screen.width * 2
+        ) {
+          var restaurantContainers = document.querySelectorAll(
+            '.restaurant-card-types'
+          );
+          restaurantContainers.forEach((container) => {
+            container.classList.add('fade-right');
+          });
+          var cafeteriaContainers = document.querySelectorAll(
+            '.cafeteria-card-types'
+          );
+          cafeteriaContainers.forEach((container) => {
+            container.classList.remove('fade-left');
+          });
         }
+        if (
+          document.body.scrollLeft + window.screen.width ===
+          window.screen.width
+        ) {
+          var cafeteriaContainers2 = document.querySelectorAll(
+            '.cafeteria-card-types'
+          );
+          cafeteriaContainers2.forEach((container) => {
+            container.classList.remove('fade-left');
+          });
+          var restaurantContainers2 = document.querySelectorAll(
+            '.restaurant-card-types'
+          );
+          restaurantContainers2.forEach((container) => {
+            container.classList.remove('fade-right');
+          });
+        }
+        document
+          .querySelector('body')
+          .scrollTo(document.body.scrollLeft + window.screen.width, 0);
+        console.log(document.body.scrollLeft + window.screen.width);
+      } else if (xDiff < -9) {
+        if (document.body.scrollLeft - window.screen.width === 0) {
+          var cafeteriaContainers3 = document.querySelectorAll(
+            '.cafeteria-card-types'
+          );
+          cafeteriaContainers3.forEach((container) => {
+            container.classList.add('fade-left');
+          });
+          var restaurantContainers3 = document.querySelectorAll(
+            '.cafeteria-card-types'
+          );
+          restaurantContainers3.forEach((container) => {
+            container.classList.remove('fade-right');
+          });
+        }
+        if (
+          document.body.scrollLeft - window.screen.width ===
+          window.screen.width
+        ) {
+          console.log('hi');
+          var cafeteriaContainers4 = document.querySelectorAll(
+            '.cafeteria-card-types'
+          );
+          cafeteriaContainers4.forEach((container) => {
+            container.classList.remove('fade-left');
+          });
+          var restaurantContainers4 = document.querySelectorAll(
+            '.restaurant-card-types'
+          );
+          restaurantContainers4.forEach((container) => {
+            container.classList.remove('fade-right');
+          });
+        }
+        console.log(document.body.scrollLeft - window.screen.width);
+        document
+          .querySelector('body')
+          .scrollTo(document.body.scrollLeft - window.screen.width, 0);
       }
       xDown = null;
-      yDown = null;
     };
     document.addEventListener('touchstart', handleTouchStart, false);
     document.addEventListener('touchmove', handleTouchMove, false);
